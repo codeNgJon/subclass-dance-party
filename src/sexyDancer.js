5891d3125d63e50187a56f0c27964e645f1498d1
Hack Reactor Students <1jonathan.ng@gmail.com>
2014-07-29 16:54:51 -0700

    Renames files

--- src/sexyDancer.js
+++
@@ -1,8 +0,0 @@
-var DogFaceDancer = function(top, left, timeBetweenSteps) {
-  Dancer.apply(this, [top, left, timeBetweenSteps]);
-  this.$node.addClass('dogFace').fadeIn();
-}
-
-DogFaceDancer.prototype = Object.create(Dancer.prototype);
-DogFaceDancer.prototype.constructor = DogFaceDancer;
-

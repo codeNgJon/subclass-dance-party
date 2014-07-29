b7efd58358d9a791d7adef4b09e16a9c03c2d5ea
Hack Reactor Students <1jonathan.ng@gmail.com>
2014-07-28 19:02:48 -0700

    New buttons for distinct dancers on the dance floor

--- src/sexyDancer.js
+++ src/sexyDancer.js
@@ -1,7 +1,6 @@
 var SexyDancer = function(top, left, timeBetweenSteps) {
   Dancer.apply(this, [top, left, timeBetweenSteps]);
-  // this.$node.addClass("sexy");
-  this.$node = $('<span class="sexy"></span>');
+  this.$node.addClass('sexy').fadeIn();
 }
 
 SexyDancer.prototype = Object.create(Dancer.prototype);
